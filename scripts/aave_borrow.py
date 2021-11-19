728a7f94b072a407f096d7ad3605e7258df19f86
codertjay <codertjay@gmail.com>
2021-11-19 15:20:14 +0100

    done with code aspect

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -15,6 +15,8 @@ def main():
         get_weth()
     lending_pool = get_lending_pool()
     print('the lending pool', lending_pool)
+    print('the lending pool address', lending_pool.address)
+    print('the account',account)
     # Approve sending out ERC20 tokens
     approve_erc20(amount, lending_pool.address, erc20_address, account)
     print("Depositing ...")
@@ -25,16 +27,78 @@ def main():
     )
     tx.wait(1)
     print("Deposited !")
+    borrowable_eth = get_borrow_data(lending_pool, account)[0]
+    print('Lets Borrow')
+    # DAI in terms of ETH
+    dai_eth_price = get_asset_price(
+        config['networks'][network.show_active()]['dai_eth_price_feed'])
+    # borrowable_eth -> borrowable_dai * 95%
+    print('the dai eth price ', dai_eth_price)
+    print('the borrowable_eth price ', borrowable_eth)
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    # Now We will borrow!
+    dai_address = config['networks'][network.show_active()
+    ]['dai_token']
+    borrow_tx = lending_pool.borrow(
+        dai_address, Web3.toWei(amount_dai_to_borrow, 'ether'),
+        1, 0, account.address, {'from': account}
+    )
+    borrow_tx.wait(1)
+    print("We borrow some DAI")
+    get_borrow_data(lending_pool, account)
+    # repay_all(amount, lending_pool, account)
+
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(Web3.toWei(amount, 'ether'), lending_pool, config['networks'][
+        network.show_active()]['dai_token'], account)
+    repay_tx = lending_pool.repay(
+        config['networks'][
+            network.show_active()]['dai_token'], amount, 1,
+        account.address, {'from': account})
+    repay_tx.wait(1)
+    print("You just deposited, borrowed and repayed with Aave, "
+          "Brownie and ChainLink")
+
+
+def get_asset_price(price_feed_address):
+    #  ABI
+    # Address
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f'The DAI/ETH price is {converted_latest_price}')
+    return float(converted_latest_price)
+
+
+def get_borrow_data(lending_pool, account):
+    (total_collateral_eth, total_debt_eth, available_borrow_eth,
+     current_liquidation_threshold, ltv,
+     health_factor) = lending_pool.getUserAccountData(account.address)
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, 'ether')
+    total_debt_eth = Web3.fromWei(total_debt_eth, 'ether')
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, 'ether')
+    current_liquidation_threshold = Web3.fromWei(current_liquidation_threshold, 'ether')
+    ltv = Web3.fromWei(ltv, 'ether')
+    health_factor = Web3.fromWei(health_factor, 'ether')
+    print(f'You have {total_collateral_eth} worth of ETH deposited.')
+    print(f'You have {total_debt_eth} worth of ETH borrowed.')
+    print(f'You have {available_borrow_eth} worth of ETH .')
+    return float(available_borrow_eth), float(total_debt_eth)
 
 
 def approve_erc20(amount, spender, erc20_address, account):
     print('Approving ERC20 Token')
-    erc20 = interface.IERC20(erc20_address)
-    tx = erc20.approve(spender, amount, {'from': account})
-    tx.wait(1)
-    print('Approved')
     # ABI
     # Address
+    erc20 = interface.IERC20(erc20_address)
+    print('pass 1')
+    print('the erc20', erc20)
+    tx = erc20.approve(spender, amount, {'from': account})
+    print('pass 1')
+    tx.wait(1)
+    print('Approved')
 
 
 def get_lending_pool():
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -5,7 +5,7 @@ from brownie import (
 
 DECIMALS = 8
 INITIAL_VALUE = 200000000000
-LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development', 'ganache-local', 'mainnet-fork']
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development', 'ganache-local', 'mainnet-fork', ]
 
 
 def get_account(index=None, id=None):
@@ -18,4 +18,6 @@ def get_account(index=None, id=None):
         return accounts.load(id)
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
     return None
